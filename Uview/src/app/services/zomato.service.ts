c93a384f3fb2a7e1ceb279810ede1699f3bc58cb
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-08-26 16:17:29 +1200

    getRestaurant method complete (i think)

--- Uview/src/app/services/zomato.service.ts
+++ Uview/src/app/services/zomato.service.ts
@@ -1,14 +1,28 @@
 ﻿import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Restaurant } from '../models/restaurant';
+
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ZomatoService {
 
     apiKey: string;
+    baseUrl: string;
 
-    constructor() {
+    constructor(private http : Http) {
         this.apiKey = "caf838732e9f9772993a3dd731d4c0a1";
+        this.baseUrl = "https://developers.zomato.com/api/v2.1/";
     }
 
-
+    getRestaurant(id: number): Observable<Restaurant> {
+        let header = new Headers();
+        header.append("user-key", this.apiKey);
+        return this.http.get(this.baseUrl + "/restaurant/" + id, { headers: header })
+            .map(res => res.json());
+    }
 
 }
