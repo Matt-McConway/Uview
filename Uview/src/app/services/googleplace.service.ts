0d847698be8eab3df6f909031d22bf85c1bbac33
SCOOBY-SURFACE\andre <andrew-lyall@hotmail.com>
2017-08-26 15:15:38 +1200

    Constructed service classes

---
+++ Uview/src/app/services/googleplace.service.ts
@@ -0,0 +1,13 @@
+﻿
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class GooglePlaceService {
+
+    apiKey: string;
+
+    constructor() {
+        this.apiKey = "AIzaSyBqcrM2hjAOuylrYp_Azo8zpb3IkDey1YU"
+    }
+
+}
---
+++ Uview/src/app/services/zomato.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ZomatoService {
+
+    apiKey: string;
+
+    constructor() {
+        this.apiKey = "caf838732e9f9772993a3dd731d4c0a1";
+    }
+
+
+
+}
