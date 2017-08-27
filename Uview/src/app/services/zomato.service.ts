14b6a1a03dada93bd78055f1f751c90869cdea4c
Parth <p.patel81@yahoo.com>
2017-08-27 13:37:03 +1200

    Third API key for the app.

--- Uview/src/app/services/zomato.service.ts
+++ Uview/src/app/services/zomato.service.ts
@@ -14,7 +14,7 @@ export class ZomatoService {
     baseUrl: string;
 
     constructor(private http : Http) {
-        this.apiKey = "fd4a59ad22e1fa80017eebb661662365";
+        this.apiKey = "6458ffa4ed70f87338a36185a8a055f6";
         this.baseUrl = "https://developers.zomato.com/api/v2.1/";
     }
 
